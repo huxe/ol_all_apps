90412932a683cd98c33503790bc52520bdc56f34
Huzaifa Ahmed <44926606+huxe@users.noreply.github.com>
2022-08-28 01:59:25 +0500

    a

---
+++ ol_batch_payment_link/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Generate Batch Payment Link",
+    'description': """ Select multiple invoices and generate one link. """,
+    "author": "Odolution",
+    "category": "CRM",
+    'currency': "USD",
+    "license": "OPL-1",
+    'price': 30.00,
+    "version": "14.0.1",
+
+    "depends": [
+        'account','sale','website_sale'
+    ],
+
+    "data": [
+        'security/ir.model.access.csv',
+        'views/main_view.xml',
+    ],
+    'images': ['static/description/background.gif'],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
